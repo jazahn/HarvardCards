21ce8902346f5c4a6528b3f8fad556ace583c394
jazahn <jcleveng@fas.harvard.edu>
2013-03-11 17:48:27 -0400

    content for splash page

--- harvardcards/protected/views/site/index.php
+++ harvardcards/protected/views/site/index.php
@@ -2,15 +2,52 @@
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<div class="row">
+	<div class="span6">
+		<p>FOR students who want to memorize words, media, or concepts and instructors who want to assess student progress.</p>
+		<p>Harvard Cards is a web application</p>
+		<p>THAT enables memorization through frequent practice and review, anytime, anywhere</p>
+		<p>UNLIKE Quizlet, Study Blue, and Anki</p>
+		<p>OUR PRODUCT has customizable exercises, games, and assessment tools that engage both instructors and students.</p>
+	</div>
+	<div class="span6">
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <tt><?php echo __FILE__; ?></tt></li>
-	<li>Layout file: <tt><?php echo $this->getLayoutFile('main'); ?></tt></li>
-</ul>
+		<div class="login-box">
+			<ul class="login-box nav nav-tabs">
+				<li class="active">
+					<a href="#">Google</a>
+				</li>
+				<li>
+					<a href="#">HUID</a>
+				</li>
+			</ul>
+			<form class="form-horizontal">
+				
+				<div class="control-group">
+					<label class="control-label" for="inputEmail">Email</label>
+					<div class="controls">
+						<input type="text" id="inputEmail" placeholder="Email" />
+					</div>
+				</div>
+				
+				<div class="control-group">
+					<label class="control-label" for="inputPassword">Password</label>
+					<div class="controls">
+						<input type="password" id="inputPassword" placeholder="Password" />
+					</div>
+				</div>
+				
+			</form>
+		</div>
+		
+	</div>
+</div>
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<div class="row">
+	<div class="span6">
+		Promotional Material
+	</div>
+	<div class="span6">
+		links, features, docs
+	</div>
+</div>

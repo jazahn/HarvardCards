3168389a0527a07abc5091d0e7b2b375e24f372f
jazahn <jcleveng@fas.harvard.edu>
2013-03-25 13:16:11 -0400

    changing adminEmail password..

--- harvardcards/protected/config/main.php
+++ harvardcards/protected/config/main.php
@@ -98,6 +98,6 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'jcleveng@fas.harvard.edu',
 	),
 );
